49c9bf2ad28952f1ce5c267f9e4df025e4bae10d
Subhendu <dncomputercentre@gmail.com>
2026-06-25 18:29:20 +0530

    Remove debug log

--- src/controllers/routinePdfController.js
+++ src/controllers/routinePdfController.js
@@ -239,12 +239,7 @@ export const generateRoutinePdf = async (req, res) => {
 
   } catch (err) {
 
-    console.log(err);
-
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
+    
 
   }
 };
