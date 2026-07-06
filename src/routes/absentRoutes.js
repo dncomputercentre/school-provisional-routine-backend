0c11c653c60f66b7d8142a9b48709b8cc31dd44f
Subhendu <dncomputercentre@gmail.com>
2026-07-06 09:25:21 +0530

    Remove debug log

--- src/routes/absentRoutes.js
+++ src/routes/absentRoutes.js
@@ -4,28 +4,12 @@ import {
   getTodayAbsentTeachers,
   resetAbsentTeacher,
   resetAllAbsentTeachers,
-  getAbsentTeacherByDate,
 } from "../controllers/absentController.js";
 
 const router = express.Router();
 
 // Mark absent
 router.post("/mark", markAbsentToday);
-
-// Get today absent teachers
-router.get("/today", getTodayAbsentTeachers);
-
-// Get absent teacher by date
-router.get(
-  "/date/:date",
-  getAbsentTeacherByDate
-);
-
-// Reset single teacher
-router.delete(
-  "/reset/:teacherId",
-  resetAbsentTeacher
-);
 
 // Reset all teachers
 router.delete(
