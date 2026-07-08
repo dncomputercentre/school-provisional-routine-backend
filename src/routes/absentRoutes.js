7b9654de007bcd78a99bd8e8de741059e751d631
Subhendu <dncomputercentre@gmail.com>
2026-07-08 19:19:04 +0530

    Restore absent routes

--- src/routes/absentRoutes.js
+++ src/routes/absentRoutes.js
@@ -8,13 +8,16 @@ import {
 
 const router = express.Router();
 
+// Get today's absent teachers
+router.get("/today", getTodayAbsentTeachers);
+
 // Mark absent
 router.post("/mark", markAbsentToday);
 
+// Reset one teacher
+router.delete("/reset/:teacherId", resetAbsentTeacher);
+
 // Reset all teachers
-router.delete(
-  "/reset-all",
-  resetAllAbsentTeachers
-);
+router.delete("/reset-all", resetAllAbsentTeachers);
 
 export default router;
