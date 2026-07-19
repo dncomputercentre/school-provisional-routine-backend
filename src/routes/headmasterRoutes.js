db3f1fde5f5dd25898960e08a2a63d75871c3bd7
Subhendu <dncomputercentre@gmail.com>
2026-07-19 09:30:00 +0530

    Temporary GET setup route

--- src/routes/headmasterRoutes.js
+++ src/routes/headmasterRoutes.js
@@ -7,16 +7,6 @@ import {
 
 const router = express.Router();
 
-// TEMP SETUP
-router.get("/setup", async (req, res) => {
-  req.body = {
-    email: "admin@gmail.com",
-    password: "12345678",
-  };
-
-  return createTempHeadMaster(req, res);
-});
-
 // Setup temp headmaster
 router.post("/setup", createTempHeadMaster);
 
